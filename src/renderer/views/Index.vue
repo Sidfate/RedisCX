b8a30a0fee27ee55059a37cd025cf3a0f2d96d46
sidfate <15757176627@163.com>
2018-05-22 09:39:19 +0800

    list all keys

--- src/renderer/views/Index.vue
+++ src/renderer/views/Index.vue
@@ -1,7 +1,6 @@
 <template>
   <el-container style="border: 1px solid #eee;height: 100%">
     <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-
       <el-menu
               class="el-menu-vertical-demo">
         <el-submenu index="1" v-for="(item, handlerIndex) in connectPool" :key="item.connectionName" >
@@ -31,7 +30,16 @@
             <el-button class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">搜索</el-button>
           </div>
 
-          <el-table :data="keys" v-loading.body="loading" element-loading-text="Loading" fit highlight-current-row style="margin: 20px 0;">
+          <el-table :data="keys" v-loading.body="loading" element-loading-text="Loading" fit highlight-current-row stripe style="margin: 20px 0;" @expand-change="onShowValue">
+            <el-table-column type="expand" >
+              <template slot-scope="props">
+                <el-form label-position="left" inline class="demo-table-expand">
+                  <el-form-item label="key">
+                    <span>{{ props.row }}</span>
+                  </el-form-item>
+                </el-form>
+              </template>
+            </el-table-column>
             <el-table-column label="key">
               <template slot-scope="scope">
                 {{scope.row}}
@@ -45,33 +53,23 @@
               </template>
             </el-table-column>
           </el-table>
-
-          <div class="pagination-container">
-            <el-pagination background @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="listQuery.pageIndex" :page-sizes="[10,20,30, 50]" :page-size="listQuery.pageSize" layout="total, sizes, prev, pager, next, jumper" :total="total">
-            </el-pagination>
-          </div>
         </div>
       </el-main>
     </el-container>
 
     <el-dialog title="连接redis" :visible.sync="dialogConnectVisible" top="0">
-      <el-form inline label-position="left" label-width="80px">
-        <el-form-item label="连接名" >
+      <el-form inline label-position="left" label-width="80px" >
+        <el-form-item label="Name" >
           <el-input v-model="connectForm.connectionName" auto-complete="off"></el-input>
         </el-form-item>
-        <el-form-item label="主机地址" >
+        <el-form-item label="Host" >
           <el-input v-model="connectForm.host" auto-complete="off"></el-input>
         </el-form-item>
-        <el-form-item label="端口" >
+        <el-form-item label="Port" >
           <el-input v-model="connectForm.port" auto-complete="off"></el-input>
         </el-form-item>
-        <el-form-item label="密码" >
-          <el-input v-model="connectForm.password" auto-complete="off" type="password">
-            <el-button slot="append" icon="el-icon-view"></el-button>
-          </el-input>
-        </el-form-item>
-        <el-form-item label="密码" v-if="viewPassword">
-          <el-input v-model="connectForm.password" auto-complete="off" suffix-icon="el-icon-view"></el-input>
+        <el-form-item label="Auth" >
+          <el-input v-model="connectForm.password" auto-complete="off" type="password"></el-input>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -107,9 +105,9 @@
         keys: [],
         viewPassword: false,
         listQuery: {
-          pageIndex: 1,
-          pageSize: 20,
-          key: ''
+          key: '',
+          cursor: '0',
+          count: '100'
         },
         total: 0
       }
@@ -152,22 +150,34 @@
       handleFilter() {
         this.fetchData()
       },
-      handleSizeChange(val) {
-        this.listQuery.pageSize = val
-        this.fetchData()
-      },
-      handleCurrentChange(val) {
-        this.listQuery.pageIndex = val
-        this.fetchData()
+      onShowValue(row, expandedRows) {
+        console.log(row)
+        console.log(expandedRows)
       },
       fetchData() {
         this.loading = true
 
-        this.selectedHandler.handler.sendCommand('SCAN 0 MATCH * COUNT 100').then(response => {
-          console.log(response)
-        })
+        const handler = this.selectedHandler.handler
+        // const get = new Redis.Command('scan', ['0'], 'utf8', function (err, result) {
+        //   console.log(result);
+        // });
 
-        this.selectedHandler.handler.keys('*').then(response => {
+        // let allKeys = []
+        // let cursor = 0
+        // const key = this.listQuery.key ? this.listQuery.key : '*'
+        // const count = this.listQuery.count
+        // do {
+        //   let scanAllKeys = new Redis.Command('scan', [cursor, 'Match', key, 'COUNT', count], {replyEncoding: 'utf8'});
+        //   scanAllKeys.promise.then(result => {
+        //     console.log(result)
+        //     cursor = result[0]
+        //     allKeys.concat(result[1])
+        //   });
+        //   handler.sendCommand(scanAllKeys)
+        // }while (cursor === 0)
+
+
+        handler.keys('*').then(response => {
           this.loading = false
           this.total = response.length
           this.keys = response

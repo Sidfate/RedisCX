ae1c9cc5befa911b0d250d8e91a21696daad4a1b
sidfate <15757176627@163.com>
2018-08-07 16:36:57 +0800

    Function: set window's minWidth and minHeight.

--- src/main/index.js
+++ src/main/index.js
@@ -18,9 +18,11 @@ function createWindow () {
    * Initial window options
    */
   mainWindow = new BrowserWindow({
-    height: 563,
+    height: 600,
     useContentSize: true,
-    width: 1000
+    width: 1000,
+    minWidth: 800,
+    minHeight: 500
   })
 
   mainWindow.loadURL(winURL)
